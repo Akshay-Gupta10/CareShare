4c17af06476667e723f346bac5237eeba4337b3d
Akshay Gupta <akshay.gupta.1018@gmail.com>
2025-09-11 18:22:14 +0530

    Update donation.js

--- models/donation.js
+++ models/donation.js
@@ -37,7 +37,7 @@ const donationSchema = new mongoose.Schema({
 	},
 	status: {
 		type: String,
-		enum: ["Pending", "Rejected", "Accepted", "Assigned", "Collected"],
+		enum: ["Pending", "Rejected", "Accepted", "Assigned", "Collected","pending", "rejected", "accepted", "assigned", "collected"],
 		required: true
 	},
 });
